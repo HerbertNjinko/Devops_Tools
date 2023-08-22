36ed496b0c30156324f2970fbfe2a69c782b76ac
herbert Abingwa <habingwa@gmail.com>
2023-08-22 16:23:15 +0000

    Update K8s terraform work flow output

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
 output "lb_endpoint" {
-  value = "http://${aws_lb.terramino.dns_name}"
+  value = "http://${aws_lb.kubernetes.dns_name}"
 }
 
 output "application_endpoint" {
-  value = "http://${aws_lb.terramino.dns_name}/index.php"
+  value = "http://${aws_lb.kubernetes.dns_name}/index.php"
 }
 
 output "asg_name" {
-  value = aws_autoscaling_group.terramino.name
+  value = aws_autoscaling_group.kubernetes.name
 }
