d5e06271fbabad4fb65fcb508c509355915f1c46
herbert Abingwa <habingwa@gmail.com>
2023-08-22 17:16:16 +0000

    Update workspace

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  shared_credentials_file = "/home/ubuntu/.aws/credentials"
+  shared_credentials_files = "/home/ubuntu/.aws/credentials"
   profile                 = "default"
   region                  = "us-west-1"
 }
