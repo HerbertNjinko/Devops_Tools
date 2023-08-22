3322cccd8410f5d215238a871a0bdda8cedd6975
herbert Abingwa <habingwa@gmail.com>
2023-08-22 17:14:50 +0000

    Update workspace

--- provider.tf
+++ provider.tf
@@ -1,4 +1,5 @@
 provider "aws" {
-  # Configuration options
-  region = "us-west-1"
+  shared_credentials_file = "/home/ubuntu/.aws/credentials"
+  profile                 = "default"
+  region                  = "us-west-1"
 }
