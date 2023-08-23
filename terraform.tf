f1a1e6aa426044d246526905158df267ab663476
herbert Abingwa <habingwa@gmail.com>
2023-08-23 00:08:47 +0000

    updated provider

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,4 @@
-/*(terraform {
+terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
@@ -12,24 +12,4 @@ provider "aws" {
   # Configuration options
   profile = "default"
   region = "us-west-1"
-}*/
-
-
-terraform {
-  cloud {
-    organization = "Abingwas-Foundation"
-
-    workspaces {
-      name = "Devops_Tools"
-    }
-  }
-  
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.13.1"
-    }
-}
-
-  required_version = ">= 0.14.0"
 }
